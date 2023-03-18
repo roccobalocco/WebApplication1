fd6faed72c34bf826fbbf151845f2aaff4d2491f
roccobalocco <pietro.masolini@studenti.unimi.it>
2023-03-18 15:18:26 +0100

    Prima struttura delle funzioni necessarie

---
+++ WebApplication1/API/Commentare.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.API;
+
+public class Commentare
+{
+    //Insert, modify, delete, reply
+    
+    public bool InserisciCommento(string commento, List<string> categorie, bool pin = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ModificaCommento(int idCommento, string commento)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool EliminaCommento(int idCommento)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RispondiCommento(int idCommento, string commento)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Utility per sopra
+    
+    //Trova tutti i tag all'interno di un commento
+    public List<int> FindTags(string commento)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TaggaUtente(int idCommento, int idUtente)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool EliminaTag(int idCommento)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Riaggiorna i tag presenti in un commento, dopo il suo aggiornamento
+    public bool AggiornaTag(int idCommento)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Ottieni commenti con vari paramentri
+
+    public DbSet<Commenti> GetCommenti()
+    {
+        return null;
+    }
+
+    public DbSet<Commenti> GetCommenti(int idUtente)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Ottieni commenti in cui un certo utente è stato taggato
+    public DbSet<Commenti> GetCommentiTag(int idUtente)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WebApplication1/API/Statistiche.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.API;
+
+public class Statistiche
+{
+    public int NumeroCommenti(int idUtente)
+    {
+        return Commentare.GetCommenti(idUtente).Count();
+    }
+
+    public int NumeroTag(int idUtente)
+    {
+        return Commentare.GetCommentiTag(idUtente).Count();
+    }
+
+    public int ComparizioniTotali(int idUtente)
+    {
+        return NumeroCommenti(idUtente) + NumeroTag(idUtente);
+    }
+
+    public DbSet<Commenti> MiglioriCommenti()
+    {
+        throw new NotImplementedException();
+    }
+
+    public DbSet<Commenti> PeggioriCommenti()
+    {
+        throw new NotImplementedException();
+    }
+}
